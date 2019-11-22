8042afabc3715fa3d3ec6acbd148b671e4d2df0f
caishukun <aniy>
2019-11-22 14:52:00 +0800

    修改登陆异常

--- src/main/java/com/meeting/meeting/config/WebConfig.java
+++ src/main/java/com/meeting/meeting/config/WebConfig.java
@@ -55,7 +55,7 @@ public class WebConfig implements WebMvcConfigurer {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("null");
+        config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config); // CORS 配置对所有接口都有效
