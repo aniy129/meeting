e8c65af25cce0f165120abb0fc9984539b4db331
caishukun <aniy>
2019-12-12 10:17:27 +0800

    用户端企业信息查询

---
+++ src/main/java/com/meeting/meeting/controller/api/user/MeetingResourceController.java
@@ -0,0 +1,43 @@
+package com.meeting.meeting.controller.api.user;
+
+import com.meeting.meeting.model.dbo.Resource;
+import com.meeting.meeting.model.dto.request.EditResourceRequest;
+import com.meeting.meeting.model.dto.request.ResourceRequest;
+import com.meeting.meeting.model.dto.response.BaseResponse;
+import com.meeting.meeting.service.ResourceService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController("UserMeetingResourceController")
+@Api(tags = "资源类型管理控制器(用户及企业端)")
+@RequestMapping("/user/meeting")
+public class MeetingResourceController {
+
+    @javax.annotation.Resource
+    private ResourceService resourceService;
+
+    @GetMapping("/resourceList")
+    @ApiOperation(value = "资源类型列表")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "pageIndex", value = "页码", required = true, dataType = "int", paramType = "query"),
+            @ApiImplicitParam(name = "pageSize", value = "每页显示数量", required = true, dataType = "int", paramType = "query")
+    })
+    public BaseResponse<Page<Resource>> resourceList(Integer pageSize, Integer pageIndex) {
+        Page<Resource> resourceList = resourceService.getResourceList(pageSize, pageIndex);
+        return BaseResponse.success(resourceList);
+    }
+
+
+    @GetMapping("/getResource")
+    @ApiOperation(value = "获取资源类型")
+    public BaseResponse<Resource> getResource(Integer id) {
+        Resource resource = resourceService.getResource(id);
+        return BaseResponse.success(resource);
+    }
+}
---
+++ src/main/java/com/meeting/meeting/controller/api/user/ResourceInfoController.java
@@ -0,0 +1,46 @@
+package com.meeting.meeting.controller.api.user;
+
+import com.meeting.meeting.model.dbo.ResourceInfo;
+import com.meeting.meeting.model.dto.request.AddResourceInfoRequest;
+import com.meeting.meeting.model.dto.request.ResourceInfoRequest;
+import com.meeting.meeting.model.dto.request.UpdateResourceInfoRequest;
+import com.meeting.meeting.model.dto.response.BaseResponse;
+import com.meeting.meeting.service.ResourceInfoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+
+@RestController("UserResourceInfoController")
+@Api(tags = "资源信息控制器(用户及企业端)")
+@RequestMapping("/user/resourceInfo")
+public class ResourceInfoController {
+    @Resource
+    private ResourceInfoService resourceInfoService;
+
+    @GetMapping("/list")
+    @ApiOperation(value = "资源类型列表")
+    public BaseResponse<Page<ResourceInfo>> resourceInfoList(@Valid ResourceInfoRequest request) {
+        Page<ResourceInfo> resourceList = resourceInfoService.getResourceInfoList(request);
+        return BaseResponse.success(resourceList);
+    }
+
+    @GetMapping("/getResource")
+    @ApiOperation(value = "获取资源信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "资源信息主键", required = true, dataType = "int", paramType = "query")
+    })
+    public BaseResponse<ResourceInfo> getResource(Integer id) {
+        ResourceInfo resourceInfo = resourceInfoService.getResource(id);
+        if (resourceInfo == null) {
+            return BaseResponse.failure("资源不存在");
+        }
+        return BaseResponse.success(resourceInfo);
+    }
+
+}
